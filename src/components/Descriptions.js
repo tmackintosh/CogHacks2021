277ed47dd55eac95f4ffdf2b617c252c739ed705
mansatCode <bhattacharyya.samuel@gmail.com>
2021-11-27 11:21:08 +0000

    powerpoint

--- src/components/Descriptions.js
+++ src/components/Descriptions.js
@@ -1,28 +1,32 @@
 import React from "react";
-import Description from "./pages/Description_Page";
+import "./Descriptions.css";
 
 function Description_Page() {
 
     return (
-        <div class="container">
-        <div class="signupboxes">
-            <h1>Software Engineer</h1>
-            <div class="row">
-                <label>Location</label><br/>
-            </div>
-            <label>Competitive salary</label><br/>
-            <div class="row">
-                <b>Job Description</b><br/>
-            </div>
-                <text>As an Engineer in our Software team, you will be responsible for the Software Engineering in developing safety-critical software for aircraft systems. You will relish architecting a design to meet the customer's requirements that stand the test of time.</text><br/>
+        <div class="scrollable">
+            <div class="container">
+                <h1>Software Engineer</h1>
+                <div class="greyRow">
+                    <label>Location</label><br/>
+                </div>
+                <div class="whiteRow">
+                    <label>Competitive salary</label><br/>
+                </div>
+                <div class="greyRow">
+                    <label><b>Job Description</b></label><br/>
+                </div>
+                <div class = "whiteRow">
+                    <text>As an Engineer in our Software team, you will be responsible for the Software Engineering in developing safety-critical software for aircraft systems. You will relish architecting a design to meet the customer's requirements that stand the test of time.</text><br/>
+                </div>
                 <div class="button">
                     <button>Go back</button>
                     <div class="divider"/>
                     <button>Apply now</button>
                 </div>
-                </div>
+            </div>
+        
         </div>
-
     )
 
 }
