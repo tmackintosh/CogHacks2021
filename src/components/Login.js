1d2526e3991cdaa8fdf5171309880d76bc3f842e
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2021-11-27 01:45:18 +0000

    Create first sign up draft

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,8 +7,23 @@ function Login() {
     
     return (
 
-        <div>
-            <h1>TEST YO</h1>
+        <div class="signupbox">
+            <img src="https://th.bing.com/th/id/OIP.k7JUAqfHqrYMpFE_NEJh2gHaE8?pid=ImgDet&rs=1"/>
+            <h1>Sign Up</h1>
+
+            <div class="row">
+                <label>Name:</label><input></input><br/>
+            </div>
+
+            <label>Email Address:</label><input></input><br/>
+
+            <div class="row">
+                <label>Postcode:</label><input></input><br/>
+            </div>
+
+            <label>Highest Level of Education:</label><input></input><br/>
+
+            <button type="button">Sign Up</button>
         </div>
 
     )
