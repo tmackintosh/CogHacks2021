5792f78cc9c621276f761a229d070a3029df782e
mansatCode <bhattacharyya.samuel@gmail.com>
2021-11-27 10:50:21 +0000

    job description page

--- src/components/Descriptions.js
+++ src/components/Descriptions.js
@@ -5,15 +5,22 @@ function Description_Page() {
 
     return (
         <div class="container">
+        <div class="signupboxes">
             <h1>Software Engineer</h1>
-            <label>Location</label><br/>
-            <label>Competitive salary</label><br/>
-            <b>Job Description</b><br/>
-            <text>As an Engineer in our Software team, you will be responsible for the Software Engineering in developing safety-critical software for aircraft systems. You will relish architecting a design to meet the customer's requirements that stand the test of time.</text><br/>
-            <div class="button">
-                <button>Go back</button>
-                <button>Apply now</button>
+            <div class="row">
+                <label>Location</label><br/>
             </div>
+            <label>Competitive salary</label><br/>
+            <div class="row">
+                <b>Job Description</b><br/>
+            </div>
+                <text>As an Engineer in our Software team, you will be responsible for the Software Engineering in developing safety-critical software for aircraft systems. You will relish architecting a design to meet the customer's requirements that stand the test of time.</text><br/>
+                <div class="button">
+                    <button>Go back</button>
+                    <div class="divider"/>
+                    <button>Apply now</button>
+                </div>
+                </div>
         </div>
 
     )
