3d930635e47098629cd4577686de83c697c2d039
Bisesh <bisesh.sitaula@gmail.com>
2021-11-27 01:16:52 +0000

    Initial Commit

---
+++ src/components/pages/Questions_Page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../Login";
+
+function Login_page() {
+
+    return (
+        <>
+            <Login/>
+        </>
+    )
+
+}
+
+export default Login_page;
