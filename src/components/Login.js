cd7b119a86ebc2ffb28c11c5c5a47f9567cc5153
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2021-11-27 02:30:22 +0000

    Add log in section

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,26 +6,49 @@ function Login() {
     
     
     return (
-
-        <div class="signupbox">
-            <img src="https://th.bing.com/th/id/OIP.k7JUAqfHqrYMpFE_NEJh2gHaE8?pid=ImgDet&rs=1"/>
-            <h1>Sign Up</h1>
+        <>
+        <div class="container">
+        <img src="https://cognisess.com/wp-content/uploads/2021/07/189.png"/>
+        <div class="signupboxes">
+            <h1><i>sign up</i></h1>
 
             <div class="row">
-                <label>Name:</label><input></input><br/>
+                <label class="row">Name:</label><input></input><br/>
             </div>
 
             <label>Email Address:</label><input></input><br/>
 
             <div class="row">
-                <label>Postcode:</label><input></input><br/>
+                <label>Password:</label><input></input><br/>
             </div>
 
-            <label>Highest Level of Education:</label><input></input><br/>
+            <label>Postcode:</label><input></input><br/>
 
-            <button type="button">Sign Up</button>
+            <div class="row">
+                <label>Highest level of education:</label><input></input>
+            </div>
+
+            <div class="button">
+                <button>Sign Up</button>
+            </div>
         </div>
 
+        <div class="signupboxes">
+            <h1><i>log in</i></h1>
+
+            <label>Email Address:</label><input></input><br/>
+
+            <div class="row">
+                <label>Password:</label><input></input><br/>
+            </div>
+
+            <div class="button">
+                <button>Log In</button>
+            </div>
+        </div>
+
+        </div>
+        </>
     )
 }
 
