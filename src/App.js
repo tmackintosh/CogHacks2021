452c908c0a36f74776dae6ab6d37ba2bb6a9bdcf
Bisesh <bisesh.sitaula@gmail.com>
2021-11-27 10:39:30 +0000

    latest

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login_page from './components/pages/Login_page';
 import Questions_Page from './components/pages/Questions_Page';
+import Job_listings_page from './components/pages/Job_listings_page';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
             <Route path='/login' exact element={<Login_page/>}/>
             <Route path='/questions' exact element={<Questions_Page/>}/>
+            <Route path='/listings' exact element={<Job_listings_page/>}/>
         </Routes>
       </Router>
     </>
---
+++ src/components/pages/Job_listings_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Job_listings from "../Job_listings";
+
+function Job_listings_page() {
+
+    return (
+        <>
+            <Job_listings/>
+        </>
+    )
+
+}
+
+export default Job_listings_page;
