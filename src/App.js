f95d2d9d1ac05818af992e47d0809b2930bade1a
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2021-11-27 01:00:12 +0000

    Configure initial components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login_page from './components/pages/Login_page';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+            <Route path='/login' exact element={<Login_page/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Login.css";
+
+function Login() {
+    
+    
+    
+    return (
+
+        <div>
+            <h1>TEST YO</h1>
+        </div>
+
+    )
+}
+
+export default Login;
